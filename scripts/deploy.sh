fd3114d7404c626c5bdf1abbbc3039fafcbd43f5
Hai Rui Zhang <zhhairui@cn.ibm.com>
2014-08-05 22:43:24 +0800

    fix bug of cache

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,7 @@ do
 done
 docker build --force-rm=true -t "$image_name" $image_dir
 if [ "$?" == "0" ]; then
-#  docker rm -f "${base_image}_container"
+  docker rm -f "${image_name}_container"
   docker run --name="${image_name}_container" -d -v $work_dir:/work_dir $ports_str  $image_name  "/bin/bash" "/work_dir/run_app"
   if [ "$?" == "0" ]; then
     echo "run image successfully!"
