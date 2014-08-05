b433a4c87b6142896bc54573f228966aba992172
zk <wsxiaozhang@gmail.com>
2014-08-05 13:07:16 +0800

    add --rm=true to remove container after running for build app

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ base_dir="$(cd "$(dirname "$0")"; pwd)"
 base_image=$1
 work_dir=$2
 docker build -t "$base_image"  $base_dir/local_image/build_image
-docker run  -i  -v $work_dir:/work_dir  $base_image  "/bin/bash" "/work_dir/build_app"
+docker run  -i --rm=true -v $work_dir:/work_dir  $base_image  "/bin/bash" "/work_dir/build_app"
 
