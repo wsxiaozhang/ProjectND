f899eaa6e4fcad489b916fa4c3eaff3c2369600b
Hai Rui Zhang <zhhairui@cn.ibm.com>
2014-08-05 11:06:42 +0800

    add build in build.sh and deploy.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ if [ "$#" -lt "2" ]; then
   echo "$0 image_name work_dir"
   exit 1
 fi
-
+base_dir="$(cd "$(dirname "$0")"; pwd)"
 base_image=$1
 work_dir=$2
-
-docker run  -i  -v $work_dir:/work_dir  $1  "/bin/bash" "/work_dir/build_app"
+docker build -t "$base_image"  $base_dir/local_image/buile_image
+docker run  -i  -v $work_dir:/work_dir  $base_image  "/bin/bash" "/work_dir/build_app"
 
--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ if [ "$#" -lt "3" ]; then
   echo "for example: $0 buildimage ./work_dir \"8080 8888:8088\""
   exit 1
 fi
+base_dir="$(cd "$(dirname "$0")"; pwd)"
 
 base_image="$1"
 work_dir="$2"
@@ -16,4 +17,5 @@ for port in $ports_arr
 do 
   ports_str="$ports_str -p $port "
 done
-docker run  -d  -v $work_dir:/work_dir $ports_str $1  "/bin/bash" "/work_dir/run_app"
+docker build -t "$base_image" $base_dir/local_image/deploy_image
+docker run  -d  -v $work_dir:/work_dir $ports_str $base_image  "/bin/bash" "/work_dir/run_app"
